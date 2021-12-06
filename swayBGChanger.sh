ddc59968f5d6b4532e3cee4de630486a658abbdd
James Leslie <jamesleslie@posteo.net>
2021-12-06 10:54:38 -0500

    [shell] Fixed killing PID
    The command to kill the PID of swaybg was incorrect. That is now fixed.

--- swayBGChanger.sh
+++ swayBGChanger.sh
@@ -2,21 +2,10 @@
 
 PATH=$bash/bin:$swaybg/bin:$PATH
 
-# swaybg -i $(find $1 -type f | shuf -n1) -m fill &
-# OLD_PID=$!
-# while true; do
-#     sleep 600
-#     swaybg -i $(find $1. -type f | shuf -n1) -m fill &
-#     NEXT_PID=$!
-#     sleep 5
-#     kill $OLD_PID
-#     OLD_PID=$NEXT_PID
-# done
-
 while true; do
     PID=`pidof swaybg`
     swaybg -i $(find $1. -type f | shuf -n1) -m fill &
     sleep 1
-    kill PID
+    kill $PID
     sleep 599
 done
