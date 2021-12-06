d412bdc1a692274e812db54df3f461cf2ef14c0c
James Leslie <jamesleslie@posteo.net>
2021-12-06 13:06:19 -0500

    [nix] Added a flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "swayBGchanger";
+        src = ./swayBGChanger.sh;
+        nativeBuildInputs = [ makeWrapper ];
+        builder = ./builder.sh;
+        # inherit swaybg;
+      };
+  };
+}
