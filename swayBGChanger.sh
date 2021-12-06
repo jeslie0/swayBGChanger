b4d829ecec6ff7795bb2f60fd78d00e310202ae6
James Leslie <jamesleslie@posteo.net>
2021-12-06 10:37:42 -0500

    [shell] Updated shell script

--- swayBGChanger.sh
+++ swayBGChanger.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
 PATH=$bash/bin:$swaybg/bin:$PATH
-swaybg -i $(find $1 -type f | shuf -n1) -m fill &
-OLD_PID=$!
+
+# swaybg -i $(find $1 -type f | shuf -n1) -m fill &
+# OLD_PID=$!
+# while true; do
+#     sleep 600
+#     swaybg -i $(find $1. -type f | shuf -n1) -m fill &
+#     NEXT_PID=$!
+#     sleep 5
+#     kill $OLD_PID
+#     OLD_PID=$NEXT_PID
+# done
+
 while true; do
-    sleep 600
+    PID=`pidof swaybg`
     swaybg -i $(find $1. -type f | shuf -n1) -m fill &
-    NEXT_PID=$!
-    sleep 5
-    kill $OLD_PID
-    OLD_PID=$NEXT_PID
+    sleep 1
+    kill PID
+    sleep 599
 done
