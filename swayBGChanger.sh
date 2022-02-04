dd58f15c1343363a3a2f2ba624529b97f6a513ef
James Leslie <jamesleslie@posteo.net>
2022-02-04 16:52:30 +0000

    [bug] Programme should now close more often when sway has been exited

--- swayBGChanger.sh
+++ swayBGChanger.sh
@@ -3,9 +3,13 @@
 PATH=$bash/bin:$swaybg/bin:$killall/bin:$PATH
 
 while true; do
-    PID=`pidof swaybg`
-    swaybg -i $(find $1 -type f | shuf -n1) -m fill &
-    sleep 1
-    kill $PID
-    sleep 599
+    if [[ $(pidof sway) == $(pidof nonexistent) ]]; then
+	break
+    else
+	PID=`pidof swaybg`
+	swaybg -i $(find $1 -type f | shuf -n1) -m fill &
+	sleep 1
+	kill $PID
+	sleep 599
+    fi
 done
