e118370dccf5680174ce9da68218d1c1a1b22be8
James Leslie <jamesleslie@posteo.net>
2022-02-05 19:17:47 +0000

    [haskell] Changing project to a haskell project
    This currently will break the flake! It will not work.

--- flake.nix
+++ flake.nix
@@ -1,17 +1,30 @@
 {
-  description = "A background changer for Swaywm";
+  description = "My Haskell project";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "swayBGchanger";
-        src = ./swayBGChanger.sh;
-        nativeBuildInputs = [ makeWrapper ];
-        builder = ./builder.sh;
-        inherit swaybg;
-      };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskellPackages;
+        packageName = "swaybgchanger";
+      in
+        {
+          packages.${packageName} = haskellPackages.callCabal2nix packageName self {};
+
+          defaultPackage = self.packages.${system}.${packageName};
+
+          devShell = pkgs.mkShell {
+            buildInputs = with haskellPackages;
+              [ ghc
+                haskell-language-server
+                cabal-install
+              ];
+            inputsFrom = builtins.attrValues self.packages.${system};
+          };
+        }
+    );
 }
--- swayBGChanger.sh
+++ swayBGChanger.sh
@@ -3,13 +3,9 @@
 PATH=$bash/bin:$swaybg/bin:$killall/bin:$PATH
 
 while true; do
-    if [[ $(pidof sway) == $(pidof nonexistent) ]]; then
-	break
-    else
-	PID=`pidof swaybg`
-	swaybg -i $(find $1 -type f | shuf -n1) -m fill &
-	sleep 1
-	kill $PID
-	sleep 599
-    fi
+    PID=`pidof swaybg`
+    swaybg -i $(find $1 -type f | shuf -n1) -m fill &
+    sleep 1
+    kill $PID
+    sleep 599
 done
