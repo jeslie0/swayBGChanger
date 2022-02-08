70c37584dfd13fd833c6f8505d2d8f119091354d
James Leslie <jamesleslie@posteo.net>
2022-02-08 11:48:18 +0000

    [haskell] Updated to a working model
    This currently will work as expected (background changing). Currently,
    I haven't tested whether or not it closes with Sway.

--- app/Main.hs
+++ app/Main.hs
@@ -2,24 +2,31 @@ module Main where
 
 import Control.Concurrent
 import System.Process
+import System.Exit
 import System.Directory
 import System.Random
 import System.Directory.Internal.Prelude (getArgs)
 
+-- | Runs the swaybg command for n seconds, then kills the thread.
 foo :: String -> Int -> IO ()
 foo str n = do
   thread <- forkIO . callCommand $ "swaybg -i " <> str
   threadDelay $ n * 1000000
   killThread thread
 
-
+-- | Gives a random element from a list, and a new generator.
 randomElement :: RandomGen g => [a] -> g -> (a, g)
-randomElement xs gen = let (n, newgen) = randomR (0, length xs) gen in (xs !! n, newgen)
+randomElement xs gen = let (n, newgen) = randomR (0, length xs - 1) gen in (xs !! n, newgen)
 
+-- | Given a list of images, one is randomly selected, then displayed
+-- on a new thread by "foo" for 10 seconds. It then loops just before
+-- the time runs out
 randomImages :: [FilePath] -> StdGen -> IO ()
 randomImages imgs gen = do
   let (randomPath, newgen) = randomElement imgs gen
-  forkIO $ foo randomPath 10
+  forkIO $ do
+    foo randomPath 10
+    print "Thread ending"
   threadDelay $ 10 * 1000000 - 1000000
   randomImages imgs newgen
 
@@ -34,7 +41,7 @@ main' dir = do
   gen <- getStdGen
   setCurrentDirectory dir
   images <- listDirectory dir
-  randomImages images gen
+  runWhenSwayIsAlive $ randomImages images gen
 
 homeFix :: FilePath -> IO FilePath
 homeFix "" = return ""
@@ -42,3 +49,25 @@ homeFix ('~':rest) = do
   home <- getHomeDirectory
   return $ home <> rest
 homeFix xs = return xs
+
+isSwayRunning :: IO Bool
+isSwayRunning = do
+  (exitcode, _, _) <- readProcessWithExitCode "pidof" ["sway"] ""
+  case exitcode of
+    ExitSuccess -> return True
+    ExitFailure _ -> return False
+
+runWhenSwayIsAlive :: IO () -> IO ()
+runWhenSwayIsAlive prog = do
+  thread <- forkIO prog
+  test thread
+    where
+      test :: ThreadId -> IO ()
+      test threadid = do
+        bool <- isSwayRunning
+        if bool
+          then do
+          threadDelay 1000000
+          test threadid
+          else
+          killThread threadid
